bac5218baa46e0ac92b430f7737f878989faf21e
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-20 23:44:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,7 @@ def badge_maker(name)
  ("Hello, my name is #{name}.")
 end  
 def batch_badge_creator(attendees)
-  new_result
-  puts badge_maker(attendees)
+  new_result = []
+  attendees.each do |name|
+    new_result << badge_maker(name)
 end 
